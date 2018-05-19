c312dea12bd73521987459e853840e6f51c0f13b
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2018-05-18 22:23:54 -0300

    [Exercicio 4] Criando cadastro de post

--- projeto/blog/Controllers/PostController.cs
+++ projeto/blog/Controllers/PostController.cs
@@ -6,14 +6,28 @@ namespace projetos.dotnet.blogCore.projeto.blog.Controllers
 {
     public class PostController : Controller
     {
-        
-        public IActionResult Index() {
-            IList<Post> lista = new List<Post>();
+        private IList<Post> lista;
+
+        public PostController()
+        {
+            lista = new List<Post>();
             lista.Add(new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" });
             lista.Add(new Post() { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" });
             lista.Add(new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" });
-            lista.Add(new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" });
+            lista.Add(new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }); 
+        }
+
+        public IActionResult Index() {
             return View(lista);
+        }
+
+        public IActionResult Novo() {
+            return View();
+        }
+
+        public IActionResult Adiciona(Post post) {
+            lista.Add(post);
+            return View("Index", lista);
         }
 
     }
